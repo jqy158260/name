0767b9fdde67e808f0da430d7e6761f6078fc860
JingQiyu <1850874153@qq.com>
2020-01-07 13:32:57 +0800

    登录

--- src/main/java/com/test/demo01/controller/TestController.java
+++ src/main/java/com/test/demo01/controller/TestController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class TestController {
 
+    private String vue;
+
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String test02() {
         return "/Hello";
